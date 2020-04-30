c743114d0a42c7bc98d6d917ededa97e5e2fb155
zingaretti.mattia <zingaretti.mattia@gmail.com>
2020-04-30 18:12:56 +0200

    Done first 2 functions

---
+++ tictactoe/errortictactoe.py
@@ -0,0 +1,4 @@
+
+class IvalidActionError(Error):
+    def __init__(self):
+        super().__init__("Invalid action attempted !")
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,11 +3,14 @@ Tic Tac Toe Player
 """
 
 import math
+from errortictactoe import InvalidActionError
 
+#Players
 X = "X"
 O = "O"
-EMPTY = None
 
+EMPTY = None
+turnCounter = 0
 
 def initial_state():
     """
@@ -22,22 +25,52 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    #Check if game is already over
+    if(terminal(board)): return None #Any value acceptable in this case
+    
+    #X moves first
+    if(board == initial_state()):
+        turnCounter += 1
+        return X
 
+    #Keep track of the sequence of turns
+    if(turnCounter%2 == 0):
+        player = O
+    else:
+        player = X
+
+    #Update total number of turns
+    turnCounter += 1
+    return player 
+    
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    #Check if game is already over
+    if(terminal(board)): return None #Any value acceptable in this case
+
+    #Initialize returning set
+    actions = set()
+
+    #Look for available actions on the board
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            # "Box" must be EMPTY to be available
+            if(board[i][j] == EMPTY):
+                set.add((i,j))
+
+    return actions
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
-
+    #Check if action is valid
+    if not action in actions(board):
+        raise InvalidActionError
 
 def winner(board):
     """
