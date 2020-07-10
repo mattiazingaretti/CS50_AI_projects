23b2ccc5d290ac98173ff8089be34d2f1620a3eb
mattiazingaretti <zingaretti.mattia@gmail.com>
2020-07-10 12:17:53 +0200

    Backup

--- pagerank/pagerank.py
+++ pagerank/pagerank.py
@@ -57,7 +57,8 @@ def transition_model(corpus, page, damping_factor):
     linked to by `page`. With probability `1 - damping_factor`, choose
     a link at random chosen from all pages in the corpus.
     """
-    raise NotImplementedError
+    
+    
 
 
 def sample_pagerank(corpus, damping_factor, n):
