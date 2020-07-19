05193eb0894b671ce50373f5bef1946cce3b8ec0
mattiazingaretti <zingaretti.mattia@gmail.com>
2020-07-19 19:59:47 +0200

    Started first function

--- crossword/generate.py
+++ crossword/generate.py
@@ -99,7 +99,10 @@ class CrosswordCreator():
         (Remove any values that are inconsistent with a variable's unary
          constraints; in this case, the length of the word.)
         """
-        raise NotImplementedError
+        for var in self.domains:
+            for value in self.domains[var]:
+                if len(value) != len(var):
+                    self.domains[var].remove(value)  
 
     def revise(self, x, y):
         """
